8e2bd7aab0b13fe91a017b54f982ab10f2ba6ce9
Nathan Kluth <nlkluth@gmail.com>
2016-04-07 14:16:44 -0600

    stubbing

--- HeroDetail.elm
+++ HeroDetail.elm
@@ -1,1 +1,35 @@
 module HeroDetail where
+
+import Html exposing (div)
+
+type alias Model = { ID: ID }
+type alias ID = Int
+
+
+init : Model
+init = fetchHero
+
+
+type Action = Fetch
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Fetch ->
+      ( if model then model else loadHero )
+
+
+loadHero : Effects Action
+loadHero =
+  Http.get decodeUrl "http://google.com"
+    |> Task.toMaybe
+    |> Effects.task
+
+
+decodeUrl : Json.Decoder String
+decodeUrl = Json.at [] Json.string
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div [] (model)
