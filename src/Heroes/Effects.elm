f775934fb9fc7a04c7ff3dcc6bc0181f330bf5dd
Nathan Kluth <nlkluth@gmail.com>
2016-04-11 13:45:11 -0600

    mmm

--- src/Heroes/Effects.elm
+++ src/Heroes/Effects.elm
@@ -21,10 +21,9 @@ fetchAllUrl =
   "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=587833f9-98ff-4cb8-92c8-ae1413a925af"
 
 
-collectionDecoder : Decode.Decoder (List (String, Hero))
+collectionDecoder : Decode.Decoder (List Hero)
 collectionDecoder =
-  Decode.object1 identity
-    ("data" := Decode.keyValuePairs memberDecoder)
+  Decode.at ["data"] (Decode.keyValuePairs memberDecoder)
 
 
 memberDecoder : Decode.Decoder Hero
