49b5465c8a1a909310bd0505d87cbc1c1c5ca15b
Nathan Kluth <nlkluth@gmail.com>
2016-05-12 13:05:57 -0600

    uhh

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
 
 require('./index.html');
 require('./styles/main.scss');
+require('localforage');
 
-var Elm = require('./Main.elm');
-var mountNode = document.getElementById('main');
+const Elm = require('./Main.elm');
+const mountNode = document.getElementById('main');
+const heroUrl = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=587833f9-98ff-4cb8-92c8-ae1413a925af";
+const app = Elm.embed(Elm.Main, mountNode);
 
-var app = Elm.embed(Elm.Main, mountNode);
+const fetchHeroList = () => {
+  localforage.getItem('heroList').then((value) => {
+    if (!value) {
+      return fetch(heroUrl);
+    }
+
+    return Promise.resolve(value)
+  }).then((value) => {
+    app.ports.heroList.send(value);
+  }).catch((error) => {
+    app.ports.errors.send(error);
+  });
+}
+
